20ce85892cffc2f6acfac2a759c957c501b31d4f
Jaikiran Pai <jpai@redhat.com>
2008-12-19 17:23:48 +0000

    Initial version of the jndibinding tutorial

---
+++ jndibinding/src/org/jboss/tutorial/jndibinding/bean/CalculatorBean.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.tutorial.jndibinding.bean;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import org.jboss.annotation.ejb.LocalBinding;
+import org.jboss.annotation.ejb.RemoteBinding;
+
+@Stateless
+@RemoteBinding(jndiBinding="Calculator")
+@LocalBinding(jndiBinding="CalculatorLocal")
+@Remote(CalculatorRemote.class)
+@Local(CalculatorLocal.class)
+public class CalculatorBean implements CalculatorRemote, CalculatorLocal
+{
+   public int add(int x, int y)
+   {
+      return x + y;
+   }
+
+   public int subtract(int x, int y)
+   {
+      return x - y;
+   }
+}
