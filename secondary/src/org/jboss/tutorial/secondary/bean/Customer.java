6204beab7155497841f35f1865bfc525a9a5c3c2
Jaikiran Pai <jpai@redhat.com>
2008-12-23 10:58:23 +0000

    Initial working version of SecondaryTable in EJB3 Entities tutorial, for JBoss-5 GA

---
+++ secondary/src/org/jboss/tutorial/secondary/bean/Customer.java
@@ -0,0 +1,136 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.tutorial.secondary.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue; import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.SecondaryTable;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER")
+@SecondaryTable(name = "EMBEDDED_ADDRESS")
+// FIXME @JoinColumn(name = "ADDRESS_ID")
+public class Customer implements java.io.Serializable
+{
+   private int id;
+   private String first;
+   private String last;
+   private String street;
+   private String city;
+   private String state;
+   private String zip;
+
+   public Customer()
+   {
+   }
+
+   public Customer(String first, String last, String street, String city, String state, String zip)
+   {
+      this.first = first;
+      this.last = last;
+      this.street = street;
+      this.city = city;
+      this.state = state;
+      this.zip = zip;
+   }
+
+   @Id @GeneratedValue(strategy=GenerationType.AUTO)
+   public int getId()
+   {
+      return id;
+   }
+
+   public void setId(int id)
+   {
+      this.id = id;
+   }
+
+   @Column(name = "FIRST")
+   public String getFirst()
+   {
+      return first;
+   }
+
+   public void setFirst(String first)
+   {
+      this.first = first;
+   }
+
+   @Column(name = "LAST")
+   public String getLast()
+   {
+      return last;
+   }
+
+   public void setLast(String last)
+   {
+      this.last = last;
+   }
+
+   @Column(name = "STREET", table = "EMBEDDED_ADDRESS")
+   public String getStreet()
+   {
+      return street;
+   }
+
+   public void setStreet(String street)
+   {
+      this.street = street;
+   }
+
+   @Column(name = "CITY", table = "EMBEDDED_ADDRESS")
+   public String getCity()
+   {
+      return city;
+   }
+
+   public void setCity(String city)
+   {
+      this.city = city;
+   }
+
+   @Column(name = "STATE", table = "EMBEDDED_ADDRESS")
+   public String getState()
+   {
+      return state;
+   }
+
+   public void setState(String state)
+   {
+      this.state = state;
+   }
+
+   @Column(name = "ZIP", table = "EMBEDDED_ADDRESS")
+   public String getZip()
+   {
+      return zip;
+   }
+
+   public void setZip(String zip)
+   {
+      this.zip = zip;
+   }
+}
