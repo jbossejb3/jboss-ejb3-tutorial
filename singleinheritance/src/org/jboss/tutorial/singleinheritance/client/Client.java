afdad4d3ae8018bb725a6db151ef57b55e96c8b4
Jaikiran Pai <jpai@redhat.com>
2008-12-23 11:18:17 +0000

    Moved the client class to the correct package and included this tutorial asa Maven project

--- singleinheritance/src/org/jboss/tutorial/singleinheritance/client/Client.java
+++ singleinheritance/src/org/jboss/tutorial/singleinheritance/client/Client.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.tutorial.singleinheritance.Client;
+package org.jboss.tutorial.singleinheritance.client;
 
 
 import org.jboss.tutorial.singleinheritance.bean.Pet;
