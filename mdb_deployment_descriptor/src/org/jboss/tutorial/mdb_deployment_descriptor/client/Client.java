5441c43eab0538b6749eb0642c6a92defb48ff53
Jaikiran Pai <jpai@redhat.com>
2008-12-26 07:10:28 +0000

    Minor changes to the MDB deployment descriptor tutorial to cleanup the connection and also add a depends element in the MDB configuration

--- mdb_deployment_descriptor/src/org/jboss/tutorial/mdb_deployment_descriptor/client/Client.java
+++ mdb_deployment_descriptor/src/org/jboss/tutorial/mdb_deployment_descriptor/client/Client.java
@@ -39,14 +39,23 @@ public class Client
       InitialContext ctx = new InitialContext();
       Queue queue = (Queue) ctx.lookup("queue/tutorial/example");
       QueueConnectionFactory factory = (QueueConnectionFactory) ctx.lookup("ConnectionFactory");
-      cnn = factory.createQueueConnection();
-      session = cnn.createQueueSession(false, QueueSession.AUTO_ACKNOWLEDGE);
-
-      TextMessage msg = session.createTextMessage("Hello World");
-
-      sender = session.createSender(queue);
-      sender.send(msg);
-      System.out.println("Message sent successfully to remote queue.");
+      try {
+         cnn = factory.createQueueConnection();
+         session = cnn.createQueueSession(false, QueueSession.AUTO_ACKNOWLEDGE);
+   
+         TextMessage msg = session.createTextMessage("Hello World");
+   
+         sender = session.createSender(queue);
+         sender.send(msg);
+         System.out.println("Message sent successfully to remote queue.");
+      } finally {
+         // Cleanup
+         // close the connection
+         if (cnn != null)
+         {
+            cnn.close();
+         }
+      }
 
    }
 }
